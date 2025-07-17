2d5b2281f0a9aa271a6eb677b3111019780f5126
Leon <echo_ai@foxmail.com>
2025-07-17 16:49:29 +0800

    docs: update

--- rspress.config.ts
+++ rspress.config.ts
@@ -15,6 +15,10 @@ export default defineConfig({
   // route: {
   //   cleanUrls: true,
   // },
+  head: [
+    '<meta name="author" content="Leon 李扬">',
+    '<script>var _hmt=_hmt || [];(function() {var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?bad2b636058313c698986f4096bb5742";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s);})();</script>'
+  ],
   themeConfig: {
     socialLinks: [
       {
@@ -33,7 +37,7 @@ export default defineConfig({
     lastUpdated: false,
     lastUpdatedText: '上次更新',
     footer: {
-      message: '<img src="/logo.svg" style="width:30px;display:inline;"><br>Dora中文社区 © 2025 <a href="/">doracc.com</a><br><a href="https://beian.miit.gov.cn/" rel="noreferrer"> 苏ICP备14007268号-19 </a> | <img src="/beian.png" style="width:12px;display:inline;"><a href="https://beian.mps.gov.cn/#/query/webSearch?code=32050602013520" rel="noreferrer"> 苏公网安备32050602013520号 </a> <script>var _hmt=_hmt || [];(function() {var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?bad2b636058313c698986f4096bb5742";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s);})();</script>',
+      message: '<img src="/logo.svg" style="width:30px;display:inline;"><br>Dora中文社区 © 2025 <a href="/">doracc.com</a><br><a href="https://beian.miit.gov.cn/" rel="noreferrer"> 苏ICP备14007268号-19 </a> | <img src="/beian.png" style="width:12px;display:inline;"><a href="https://beian.mps.gov.cn/#/query/webSearch?code=32050602013520" rel="noreferrer"> 苏公网安备32050602013520号 </a>',
     },
   },
   markdown: {
