62d2b06ebc708f3382cc926b59662c0b5658a9fc
Leon <echo_ai@foxmail.com>
2025-07-18 21:42:59 +0800

    feat: custom theme

--- theme/index.tsx
+++ theme/index.tsx
@@ -10,28 +10,28 @@ const Layout = () => {
         hm.src = "https://hm.baidu.com/hm.js?bad2b636058313c698986f4096bb5742";
         hm.type = "text/javascript";
 
-        let gitcus = document.createElement("script");
-        gitcus.src = "https://giscus.app/client.js";
-        gitcus.setAttribute("data-repo", "DoraCN/doracc.com");
-        gitcus.setAttribute("data-repo-id", "R_kgDOPMxSoA");
-        gitcus.setAttribute("data-category", "Announcements");
-        gitcus.setAttribute("data-category-id", "DIC_kwDOPMxSoM4CtH7d");
-        gitcus.setAttribute("data-mapping", "pathname");
-        gitcus.setAttribute("data-strict", "0");
-        gitcus.setAttribute("data-reactions-enabled", "1");
-        gitcus.setAttribute("data-emit-metadata", "0");
-        gitcus.setAttribute("data-input-position", "bottom");
-        gitcus.setAttribute("data-theme", "preferred_color_scheme");
-        gitcus.setAttribute("data-lang", "zh-CN");
-        gitcus.setAttribute("data-loading", "lazy");
-        gitcus.crossOrigin = "anonymous";
-        gitcus.async = true;
+        // let gitcus = document.createElement("script");
+        // gitcus.src = "https://giscus.app/client.js";
+        // gitcus.setAttribute("data-repo", "DoraCN/doracc.com");
+        // gitcus.setAttribute("data-repo-id", "R_kgDOPMxSoA");
+        // gitcus.setAttribute("data-category", "Announcements");
+        // gitcus.setAttribute("data-category-id", "DIC_kwDOPMxSoM4CtH7d");
+        // gitcus.setAttribute("data-mapping", "pathname");
+        // gitcus.setAttribute("data-strict", "0");
+        // gitcus.setAttribute("data-reactions-enabled", "1");
+        // gitcus.setAttribute("data-emit-metadata", "0");
+        // gitcus.setAttribute("data-input-position", "bottom");
+        // gitcus.setAttribute("data-theme", "preferred_color_scheme");
+        // gitcus.setAttribute("data-lang", "zh-CN");
+        // gitcus.setAttribute("data-loading", "lazy");
+        // gitcus.crossOrigin = "anonymous";
+        // gitcus.async = true;
 
         setTimeout(() => {
           const root = document.getElementById('root');
           if (root) {
               root.appendChild(hm);
-              root.appendChild(gitcus);
+              // root.appendChild(gitcus);
           }
         }, 0);
 
@@ -47,7 +47,7 @@ const Layout = () => {
             <p><a href="https://beian.miit.gov.cn/" rel="noreferrer"> 苏ICP备14007268号-19 </a> | <img src="/beian.png" style={{ width: '12px', display: 'inline' }} /><a href="https://beian.mps.gov.cn/#/query/webSearch?code=32050602013520" rel="noreferrer"> 苏公网安备32050602013520号 </a></p>
           </div>
         }
-        afterDocFooter={<div className='giscus giscus-frame'></div>}
+        // afterDocFooter={<div className='giscus giscus-frame'></div>}
       />
     );
 };
