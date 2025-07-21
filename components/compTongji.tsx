69c2cc83f883a876a92811118c6ed8c6e7e68eb2
Leon <echo_ai@foxmail.com>
2025-07-21 17:25:30 +0800

    docs: update

--- components/compTongji.tsx
+++ components/compTongji.tsx
@@ -1,25 +1,11 @@
-import { useEffect, useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { useLocation } from "rspress/runtime";
 
 
 export default function PluginTongji() {
     const { pathname } = useLocation();
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //     console.log(pathname);
-    //     }, 0);
-    // }, [pathname]);
-
-    useLayoutEffect(() => {
-        // const script = document.createElement("script");
-        // script.src = "https://hm.baidu.com/hm.js?bad2b636058313c698986f4096bb5742";
-        // script.async = true;
-        // document.body.appendChild(script);
-        // return () => {
-        //     document.body.removeChild(script);
-        // };
-        // console.log(pathname);
+    useEffect(() => {
         if (typeof _hmt !== 'undefined') {
             _hmt.push(['_trackPageview', pathname]);
         }
